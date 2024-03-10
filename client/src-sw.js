011a498fae385a75d5db09d7b7abf3c5cdda38b4
Deboh12 <deborah.alexander12@yahoo.com>
2024-03-10 00:02:32 -0600

    implemented asset caching to src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) =>
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'image',
+  new StaleWhileRevalidate({
+    cacheName: 'assets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
